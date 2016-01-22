9813018bdf96d7a5e94ec7a608caca5d3c7fdc49
Pieter Hordijk <info@pieterhordijk.com>
2016-01-22 20:04:54 +0100

    Fixed stupid

--- cli/run.php
+++ cli/run.php
@@ -2,14 +2,15 @@
 
 namespace Room11\Platter\Cli;
 
+use Aerys\Host;
+
 require_once __DIR__ . '/../bootstrap.php';
 
 foreach ($hosts as $name => $host) {
+    (new Host)
+        ->name($name)
+        ->expose($host[0], $host[1])
+        ->use($router)
+        ->use(\Aerys\root(__DIR__ . '/../public'))
+    ;
 }
-
-(new \Aerys\Host)
-    ->name($name)
-    ->expose($host[0], $host[1])
-    ->use($router)
-    ->use(\Aerys\root(__DIR__ . '/../public'))
-;
